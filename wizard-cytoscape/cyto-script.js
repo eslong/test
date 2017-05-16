281f46d19dd755aa8f91213a0599ed3a4f72a878
eslong <long.etienne@gmail.com>
2017-05-16 18:33:20 -0400

    Added newer version of code.

--- wizard-cytoscape/cyto-script.js
+++ wizard-cytoscape/cyto-script.js
@@ -1,94 +1,187 @@
-/*Counter for the ID of the next Node, to be used in generating the node's ID and the index for data relating to the node to be added.*/
+//Counter for the ID of the next Node, to be used in generating the node's ID.
 var nodeID = 1;
+//Variable to check if the shift key is being pressed.
+var shifted = false;
+var controlled = false;
 
-/*Object that will store any data relating to the Node.*/
-function Data(id, label, description) {
-	this.id = id;
-	this.label = label;
-	this.description = description;
-}
+var isNodeEdit = false;
+var isNodeAdd = false;
 
-/*Array that will store the created Data objects by index relating to the ID of their respective Nodes.*/
-var arbNodeData = [];
+var addPosX;
+var addPosY;
+var parentNode;
 
-/*Data for the base Node.*/
-arbNodeData[0] = new Data('0', 'Base Node', 'This is the base Node for the graph.');
+var currentNode = null;
 
-/*Function that handles operations and settings for the graph.*/
+var defaultURI = 'http://thoughtwire.ca/demo/MedicalWard';
+
+//Function that handles operations and settings for the graph.
 $(function(){
+	
+	//Event handlers to check if the Shift key or the Control key is being held.
+	var SHIFT = 16;
+	var CTRL = 17;
+	
+	$(document).keydown(function(e) {
+		if (e.keyCode == SHIFT) {
+			shifted = true;
+		} else if (e.keyCode == CTRL) {
+			controlled = true;
+		}
+	});
+	
+	$(document).keyup(function(e) {
+		if (e.keyCode == SHIFT) {
+			shifted = false;
+		} else if (e.keyCode == CTRL) {
+			controlled = false;
+		}
+	});
+	
+	$('#dialog').dialog({
+		autoOpen: false,
+		modal: true,
+		width: 550,
+		height: 600
+	});
+	
+	$('#dialog-submit').click(function(e) {
+		e.preventDefault();
+		if (isNodeEdit) {
+			editNode();
+		} else if (isNodeAdd) {
+			addNode();
+		}
+		$('#dialog').dialog('close');
+	});
+	
 	//An instance of Cytoscape is created within the div with the id 'cy'
 	$('#cy').cytoscape({
 		
-		/*Specifies the layout to use (once I can get Arbor to work, that will be the layout used).*/
+		//Specifies the layout to use (once I can get Arbor to work, that will be the layout used).
 		layout: { name: 'grid', padding: 430, zoom: 1.0 },
 		
-		/*Disabled to improve formatting.*/
+		//Disabled to improve formatting.
 		zoomingEnabled: false,
 		
-		/*Style settings for the graph and it's elements.*/
+		//Style settings for the graph and it's elements.
 		style: cytoscape.stylesheet()
 		
-			/*Styling for all nodes.*/
+			//Styling for all nodes.
 			.selector('node')
 				.css({
-						'content': 'data(name)',
-						'text-valign': 'center',
+						'content': 'data(label)',
+						'text-valign': 'bottom',
+						'shape': 'rectangle',
+						'background-color': 'white',
 						'color': 'white',
-						'background-color': 'pink',
+						'background-image': 'collaboration2.png',
+						'background-fit': 'contain',
 						'text-outline-width': 2,
-						'text-outline-color': '#888'
+						'text-outline-color': 'gray'
 				})
 				
-			/*Styling for all edges.*/
-			.selector('edge').css( { 'line-color': 'black' } )
+			//Styling for all edges.
+			.selector('edge')
+				.css({ 
+						'line-color': 'black', 
+						'content': 'data(label)',
+						'color': 'white',
+						'text-valign': 'center',
+						'text-outline-width': 2,
+						'text-outline-color': 'black'
+					})
 			
-			/*Styling for all selected items.*/
+			//Styling for all selected items.
 			.selector(':selected')
 				.css({
-					'border-color':'black',
+					'border-color': 'black',
 					'line-color':'blue',
 					'target-arrow-color':'blue'
 				}),
 				
-		/*Add the initial node for the graph.*/
-		elements: { nodes: [ { data: { id: '0', name: 'Base Node' } } ] },
+		//Add the initial node for the graph.
+		elements: { nodes: [ { data: { id: '0', label: 'Collaboration', group: 'Collaboration', uri: defaultURI, description: 'This is the base node for the graph.' } } ] },
 		
-		/*Function that handles what actions the graph can take.*/
+		//Function that handles what actions the graph can take.
 		ready: function() {
 			window.cy = this;
-			var currentNode = null;
 			var tooltipHidden = true;
+			var hiddenNodes;
 			
-			/*Events for qTip*/
+			//Events for qTip
 			cy.on('tap', 'node', function(e) {
 				var targetNode = e.cyTarget;
 				var targetID = targetNode.data('id');
-				var targetLbl = targetNode.data('name');
+				var targetLabel = targetNode.data('label');
+				var targetGroup = targetNode.data('group');
+				var targetURI = targetNode.data('uri');
+				var targetDesc = targetNode.data('description');
 				var x = e.cyPosition.x;
                 var y = e.cyPosition.y;
                 
-                /*If the node you clicked has an open tooltip...*/
-				if ( currentNode === targetNode ) {
-					/*...show it if it has been hidden.*/
+                var cHidden = false;
+                
+                if ( shifted ) {
+                	$('#dialog').dialog('open');
+        			
+        			document.getElementById('dialog-title').innerHTML = 'Edit Node';
+        			document.getElementById('dialog-id-label').innerHTML = 'ID: ';
+        			document.getElementById('dialog-id').innerHTML = targetID;
+        			document.getElementById('dialog-name').value = targetLabel;
+        			if ( targetGroup == 'Collaboration' ) {
+        				document.getElementById('dialog-type-collaboration').checked = true;
+        			} else if ( targetGroup == 'Controller' ) {
+        				document.getElementById('dialog-type-controller').checked = true;
+        			} else if ( targetGroup == 'Database' ) {
+        				document.getElementById('dialog-type-database').checked = true;
+        			}
+        			document.getElementById('dialog-uri').value = targetURI;
+        			document.getElementById('dialog-description').value = targetDesc;
+        			
+        			isNodeEdit = true;
+        			isNodeAdd = false;
+        			
+                	shifted = false;
+                } else if ( controlled ) {
+                	var childEdges = cy.elements( 'edge[source="' + targetID + '"]' );
+                	var childNodes = childEdges.targets();
+                	if (cHidden) {
+                		hiddenNodes.show();
+                		cHidden = false;
+                	} else {
+                		childNodes.hide();
+                		hiddenNodes = childNodes;
+                		cHidden = true;
+                	}
+                	controlled = false;
+                } else if ( currentNode === targetNode ) {    //If the node you clicked has an open tooltip...
+					//...show it if it has been hidden.
 					if (tooltipHidden) {
-						$('#tooltip').qtip('show');
+						addTooltip();
 						tooltipHidden = false;
-					/*...hide it if it is not already.*/
+					//...hide it if it is not already.
 					} else {
-						$('#tooltip').qtip('hide');
+						$('#tooltip').qtip('destroy');
 						tooltipHidden = true;
 					}
-				/*If you clicked on a different node, create and show the tooltip with the node's information.*/
+				//If you clicked on a different node, create and show the tooltip with the node's information.
 				} else {   
-					if ( $('#tooltip').data('qtip') ) $('#tooltip').qtip('destroy');   //Remove any previous tooltips.
+					addTooltip();
+				}
+                
+                function addTooltip() {
+                	if ( $('#tooltip').data('qtip') ) $('#tooltip').qtip('destroy');   //Remove any previous tooltips.
 					
-					$('#tooltip').css({left: x, top: y - 2});   //Position the tooltip
+					$('#tooltip').css({left: x - 2, top: y + 4});   //Position the tooltip
 					
 					//Creating the tooltip (content, positioning, and styling)
 					$('#tooltip').qtip({
 						content: '<strong>ID:</strong> ' + targetID + 
-								 '</br><strong>Label:</strong> ' + targetLbl + 
-								 '</br><strong>Description: </strong> ' + arbNodeData[ parseInt(targetID) ].description,
+								 '</br><strong>Label:</strong> ' + targetLabel +
+								 '</br><strong>Type: </strong> ' + targetGroup +
+								 '</br><strong>URI: </strong> <a href="http://www.thoughtwire.com">' + targetURI + '</a>' +
+								 '</br><strong>Description: </strong> ' + targetDesc ,
 						show: {
 							ready: true
 						},
@@ -105,61 +198,119 @@ $(function(){
 						}
 					});
 					
-					currentNode = targetNode;
 					tooltipHidden = false;
-				}
+                }
+                
+                currentNode = targetNode;
 			});
 		
-			/*Event handler for adding Nodes when an existing Node is right-clicked*/
+			//Event handler for adding Nodes when an existing Node is right-clicked.
 			cy.on('cxttap', 'node', function(e) {
-				var source = e.cyTarget;
-				addNode( source.data('id'), source.renderedPosition( 'x' ), source.renderedPosition( 'y' ) );
+				var target = e.cyTarget;
+				var targetID = target.data('id');
+				
+				$('#dialog').dialog('open');
+    			
+    			document.getElementById('dialog-title').innerHTML = 'Add Node';
+    			document.getElementById('dialog-id-label').innerHTML = 'Parent: ';
+    			document.getElementById('dialog-id').innerHTML = targetID;
+    			document.getElementById('dialog-name').value = '';
+    			document.getElementById('dialog-type-collaboration').checked = true;
+    			document.getElementById('dialog-uri').value = defaultURI;
+    			document.getElementById('dialog-description').value = '';
+    			
+    			isNodeEdit = false;
+    			isNodeAdd = true;
+    			
+    			addPosX = target.renderedPosition('x');
+    			addPosY = target.renderedPosition('y');
+    			parentNode = targetID;
+			});
+			//Event handler for editing an Edge when it is shift-clicked.
+			cy.on('tap', 'edge', function(e) {
+				if (shifted) {
+					var target = e.cyTarget;
+					var edgeLabel = prompt( 'Enter a name for the edge:', target.data('label') );
+					target.data('label', edgeLabel);
+					shifted = false;
+				}
 			});
 		}
 	});
 });
 
-/*Function that handles to adding of Nodes and Edges to the graph.*/
-function addNode( parentNode, x, y ) {
+//Function that handles to adding of Nodes and Edges to the graph.
+function addNode() {
 	var newNodeID = nodeID.toString();
-	var newNodeLabel = prompt('Enter a name to label your new Node:', 'Node ' + nodeID);
+	var newNodeLabel = document.getElementById('dialog-name').value;
+	var newNodeGroup;
+	if ( document.getElementById('dialog-type-collaboration').checked ) {
+		newNodeGroup = 'Collaboration';
+	} else if ( document.getElementById('dialog-type-controller').checked ) {
+		newNodeGroup = 'Controller';
+	} else if ( document.getElementById('dialog-type-database').checked ) {
+		newNodeGroup = 'Database';
+	}
+	var newNodeURI = document.getElementById('dialog-uri').value;
+	var newNodeDesc = document.getElementById('dialog-description').value;
 	var newEdge = parentNode + '->' + newNodeID;
 	
 	//Positions the new Nodes in random positions around the parent Node.
-	var nodeX = x + Math.floor( Math.random() * 300 - 150);
-	var nodeY = y + Math.floor( Math.random() * 300 - 150);
+	var nodeX = addPosX + Math.floor( Math.random() * 300 - 150);
+	var nodeY = addPosY + Math.floor( Math.random() * 300 - 150);
 	
 	//Add the new Node and the Edge that connects it to it's parent.
 	cy.add([
-	    { group: 'nodes', data: { id: newNodeID, name: newNodeLabel }, renderedPosition: { x: nodeX, y: nodeY } },
-		{ group: 'edges', data: { id: newEdge, source: parentNode, target: newNodeID } }
+	    { group: 'nodes', data: { id: newNodeID, label: newNodeLabel, group: newNodeGroup, uri: newNodeURI, description: newNodeDesc }, renderedPosition: { x: nodeX, y: nodeY } },
+		{ group: 'edges', data: { id: newEdge, source: parentNode, target: newNodeID, label: 'hasPart' } }
 	]);
 	
-	//Check what type of node the user would like to create.
-	nodeGroup();
+	var target = cy.elements('node#' + newNodeID);
 	
-	//Add a description to the created node.
-	var newNodeDesc = prompt('Enter a description for your new Node:', 'Node ' + nodeID);
-	arbNodeData[nodeID] = new Data(newNodeID, newNodeLabel, newNodeDesc);
-	
-	//Function for checking Node type and changing the Node's properties accordingly.
-	function nodeGroup() {
-		var group = prompt('What type of node would you like: Alpha or Beta?', 'Alpha').toLowerCase();
-		if (group === null || group === '') {
-			return;
-		} else if (group === 'alpha') {
-			cy.elements('node#' + newNodeID).css( { 'shape': 'triangle', 'background-color': 'green', 'text-outline-color': 'red' } );
-		} else if (group === 'beta') {
-			cy.elements('node#' + newNodeID).css( { 'shape': 'rectangle', 'background-color': 'orange', 'text-outline-color': 'purple' } );
-		} else {
-			alert('That is not a valid type for a node! Please choose between the following: Alpha or Beta.');
-			nodeGroup();
-		}
+	if (newNodeGroup == 'Collaboration') {
+		target.css( { 'background-image': 'collaboration2.png', 'text-outline-color': 'gray' } );
+	} else if (newNodeGroup == 'Controller') {
+		target.css( { 'background-image': 'controller2.png', 'text-outline-color': 'purple' } );
+	} else if (newNodeGroup == 'Database') {
+		target.css( { 'background-image': 'database.png', 'text-outline-color': 'red' } );
 	}
 	
 	//Increment the ID for the next Node.
 	++nodeID;
 }
+
+function editNode() {
+	var newNodeLabel = document.getElementById('dialog-name').value;
+	var newNodeGroup;
+	if ( document.getElementById('dialog-type-collaboration').checked ) {
+		newNodeGroup = 'Collaboration';
+	} else if ( document.getElementById('dialog-type-controller').checked ) {
+		newNodeGroup = 'Controller';
+	} else if ( document.getElementById('dialog-type-database').checked ) {
+		newNodeGroup = 'Database';
+	}
+	var newNodeURI = document.getElementById('dialog-uri').value;
+	var newNodeDesc = document.getElementById('dialog-description').value;
+	
+	currentNode.data('label', newNodeLabel);
+	currentNode.data('group', newNodeGroup);
+	currentNode.data('uri', newNodeURI);
+	currentNode.data('description', newNodeDesc);
+	
+	if (newNodeGroup == 'Collaboration') {
+		currentNode.css( { 'background-image': 'collaboration2.png', 'text-outline-color': 'gray' } );
+	} else if (newNodeGroup == 'Controller') {
+		currentNode.css( { 'background-image': 'controller2.png', 'text-outline-color': 'purple' } );
+	} else if (newNodeGroup == 'Database') {
+		currentNode.css( { 'background-image': 'database.png', 'text-outline-color': 'red' } );
+	}
+}
+
+
+
+
+
+
 
 
 
--- wizard-vis/vis-script.js
+++ wizard-vis/vis-script.js
@@ -1,109 +1,239 @@
-/*DataSet that will hold any arbitrary data linked to nodes*/
-var arbNodeData = new vis.DataSet();
-arbNodeData.add( [ { id: 0, label: 'Base Node', description: 'This is the base node for the graph.'} ] );
 
-/*DataSet that will store the nodes of the graph*/
+var defaultURI = 'http://thoughtwire.ca/demo/MedicalWard';
+
+//DataSet that will hold any arbitrary data linked to nodes
+var arbNodeData = new vis.DataSet();
+arbNodeData.add( [ { id: 0, label: 'Collaboration', group: 'Collaboration', uri: defaultURI, description: 'This is the base node for the graph.'} ] );
+
+//DataSet that will hold any arbitrary data linked to edges.
+var arbEdgeData = new vis.DataSet();
+
+//DataSet that will store the nodes of the graph
 var nodes = new vis.DataSet();
 
-/*Creating the initial Node*/
-nodes.add( [ { id: 0, label: arbNodeData.get(0).label, group: 'Omega', title: '<strong>ID:</strong> 0</br><strong>Label:</strong>' +  arbNodeData.get(0).label + '</br><strong>Description:</strong> ' + arbNodeData.get(0).description } ] );
+//Creating the initial Node
+nodes.add( [ { id: 0, label: arbNodeData.get(0).label, group: 'Collaboration', title: '<strong>ID:</strong> 0</br><strong>Name:</strong>' +  arbNodeData.get(0).label + 
+																					  '<br><strong>Type:</strong> Collaboration' +
+																					  '</br><strong>URI:</strong> <a href="http://www.thoughtwire.com">' + arbNodeData.get(0).uri + '</a>' +
+																					  '</br><strong>Description:</strong> ' + arbNodeData.get(0).description } ] );
 
-/*DataSet that will store the edges of the graph*/
+//DataSet that will store the edges of the graph
 var edges = new vis.DataSet();
 
-/*Stores what the Network will be contained within*/
+//Stores what the Network will be contained within
 var container = document.getElementById('graph');
 
-/*Object that contains the nodes and edges*/
+//Object that contains the nodes and edges
 var data = {
 	nodes: nodes,
 	edges: edges,
 };
 
-/*Object for formatting the Network*/
+//Object for formatting the Network
 var options = {
 	stabilize: false,
 	dragNetwork: false,
+	zoomable: false,
+	
+	// Interface for editing nodes and moving edges that comes with the library, disabled by default.
 	/*dataManipulation: {
 		enabled: true,
 		initiallyVisible: true
 	},*/
+	
 	edges: {
 		style: 'line',
-		/*Does not appear to work (highlighting an edge should turn it red)*/
+		inheritColor: false,
 		color: {
-			highlight: 'red'
+			highlight: 'blue'
 		}
 	},
 	groups: {
-		Omega: {
-			shape: 'star',
-			color: 'orange'
+		Collaboration: {
+			shape: 'image',
+			image: 'collaboration2.png',
+			fontSize: '16'
 		},
-		Alpha: {
-			shape: 'triangle',
-			color: 'gray'
+		Controller: {
+			shape: 'image',
+			image: 'controller2.png'
 		},
-		Beta: {
-			shape: 'triangleDown',
-			color: 'green'
+		Database: {
+			shape: 'image',
+			image: 'database.png'
 		}
 	}
 };
 
-/*Function for adding nodes when the graph detects a mouse click.*/
+var isNodeEdit = false;
+var isNodeAdd = false;
+
+//Function for adding nodes when the graph detects a mouse click.
 var newNodeID = 0;
-function addNode(parent) {
-	var newNodeGroup = checkGroup();
-	if (newNodeGroup === 'Cancel') { return; };
+var newEdgeID = 100000;
+function addNode() {
 	++newNodeID;
-	var newNodeLbl = checkInput('Enter a label for the new Node:');
-	var newNodeDesc = checkInput('Enter a description for the new Node:');
 	
-	/*Checks that the choice of group is valid*/
-	function checkGroup() {
-		var nodeGroup = prompt('What type of Node would you like to create: Alpha or Beta?', 'Alpha').toLowerCase();
-		if (nodeGroup === null || nodeGroup === '') {
-			return 'Cancel';
-		} else if (nodeGroup === 'alpha'){
-			return 'Alpha';
-		} else if (nodeGroup === 'beta') {
-			return 'Beta';
-		} else {
-			alert('That is not a valid group! Please choose between the following: Alpha or Beta.');
-			checkGroup();
-		}
+	var parentNode = document.getElementById('dialog-id').innerHTML;
+	var newNodeLabel = document.getElementById('dialog-name').value;
+	var newNodeGroup;
+	if ( document.getElementById('dialog-type-collaboration').checked ) {
+		newNodeGroup = 'Collaboration';
+	} else if ( document.getElementById('dialog-type-controller').checked ) {
+		newNodeGroup = 'Controller';
+	} else if ( document.getElementById('dialog-type-database').checked ) {
+		newNodeGroup = 'Database';
 	}
+	var newNodeURI = document.getElementById('dialog-uri').value;
+	var newNodeDesc = document.getElementById('dialog-description').value;
 	
-	/*Checks that the input from the user is valid*/
-	function checkInput(query, isLabel) {
-		var initVal = 'Node' + newNodeID;
-		
-		var input = prompt(query, initVal);
-		if (input === '' || input === null) {
-			alert('Invalid input, please try again.');
-			checkInput(query, isLabel);
-		} else {
-			return input;
-		}
-	}
-	/*Adds the node and the data associated with it to their respective DataSets*/
-	arbNodeData.add( [ { id: newNodeID, label: newNodeLbl, description: newNodeDesc } ] );
-	nodes.add( [ { id: newNodeID, label: newNodeLbl, group: newNodeGroup,  title: '<strong>ID:</strong> ' + newNodeID + 
-																				  '</br><strong>Label:</strong> ' + newNodeLbl + 
+
+	//Adds the node and the data associated with it to their respective DataSets
+	arbNodeData.add( [ { id: newNodeID, label: newNodeLabel,group: newNodeGroup, uri: newNodeURI, description: newNodeDesc } ] );
+	nodes.add( [ { id: newNodeID, label: newNodeLabel, group: newNodeGroup,  title: '<strong>ID:</strong> ' + newNodeID + 
+																				  '</br><strong>Label:</strong> ' + arbNodeData.get(newNodeID).label + 
+																				  '</br><strong>Type:</strong> ' + arbNodeData.get(newNodeID).group+
+																				  '</br><strong>URI:</strong> <a href="http://www.thoughtwire.com">' + arbNodeData.get(newNodeID).uri + '</a>' +
 																				  '</br><strong>Description:</strong> ' + arbNodeData.get(newNodeID).description } ] );
-	/*Only adds an edge if their is a parent node to connect (nodes can therefore be added by dblclicking on any part of the canvas without causing errors)*/
-	if (parent !== null) {
-		edges.add( [ { from: newNodeID, to: parent } ] );
-	};
+	//Add the edge linking the node to it's parent.
+	++newEdgeID;
+	edges.add( [ { id: newEdgeID, from: newNodeID, to: Number(parentNode), label: 'hasPart' } ] );
 };
 
-/*Creates the Network*/
+//Function for editing the labels of edges.
+function editEdge(target) {
+	var edgeLabel = prompt( 'Enter a name for the edge: ', edges.get(target).label );
+	edges.update({
+		id: target,
+		label: edgeLabel
+	});
+}
+
+//Function for editing the properties of a node.
+function editNode() {
+	var targetNode = document.getElementById('dialog-id').innerHTML;
+	var nodeLabel = document.getElementById('dialog-name').value;
+	var nodeGroup;
+	if ( document.getElementById('dialog-type-collaboration').checked ) {
+		nodeGroup = 'Collaboration';
+	} else if ( document.getElementById('dialog-type-controller').checked ) {
+		nodeGroup = 'Controller';
+	} else if ( document.getElementById('dialog-type-database').checked ) {
+		nodeGroup = 'Database';
+	}
+	var nodeURI = document.getElementById('dialog-uri').value;
+	var nodeDesc = document.getElementById('dialog-description').value;
+	
+	arbNodeData.update({
+		id: targetNode,
+		label: nodeLabel,
+		group: nodeGroup,
+		uri: nodeURI,
+		description: nodeDesc
+	});
+	
+	nodes.update({
+		id: targetNode,
+		label: nodeLabel,
+		group: nodeGroup,
+		title:'<strong>ID:</strong> ' + newNodeID + 
+		  '</br><strong>Label:</strong> ' + arbNodeData.get(newNodeID).label + 
+		  '</br><strong>Type:</strong> ' + arbNodeData.get(newNodeID).group+
+		  '</br><strong>URI:</strong> <a href="http://www.thoughtwire.com">' + arbNodeData.get(newNodeID).uri + '</a>' +
+		  '</br><strong>Description:</strong> ' + arbNodeData.get(newNodeID).description
+	});
+}
+
+//Creates the Network
 var network = new vis.Network(container, data, options);
 
-/*Create child nodes when double-clicking a node*/
-network.on('doubleClick', function (properties) {
-	addNode(properties.nodes);
+//Handlers for shift-clicking.
+var shifted = false;
+
+$(function(){
+	//Event handlers to check if the Shift key is being held.
+	var SHIFT = 16;
+	
+	$(document).keydown(function(e) {
+		if (e.keyCode == SHIFT) {
+			shifted = true;
+		}
+	});
+	
+	$(document).keyup(function(e) {
+		if (e.keyCode == SHIFT) {
+			shifted = false;
+		}
+	});
+	
+	$('#dialog').dialog({
+		autoOpen: false,
+		modal: true,
+		width: 550,
+		height: 600
+	});
+	
+	$('#dialog-submit').click(function(e) {
+		e.preventDefault();
+		if (isNodeEdit) {
+			editNode();
+		} else if (isNodeAdd) {
+			addNode();
+		}
+		$('#dialog').dialog('close');
+	});
+});
+
+
+//Create child nodes when double-clicking a node
+network.on('doubleClick', function(properties) {
+	var node = properties.nodes[0];
+	if (node != '') {
+		$('#dialog').dialog('open');
+		
+		document.getElementById('dialog-title').innerHTML = 'Add Node';
+		document.getElementById('dialog-id-label').innerHTML = 'Parent: ';
+		document.getElementById('dialog-id').innerHTML = nodes.get(node).id;
+		document.getElementById('dialog-name').value = '';
+		document.getElementById('dialog-type-collaboration').checked = true;
+		document.getElementById('dialog-uri').value = defaultURI;
+		document.getElementById('dialog-description').value = '';
+		
+		isNodeAdd = true;
+		isNodeEdit = false;
+	}
+});
+
+//Edit the label of an edge when it is shift-clicked.
+network.on('click', function(properties) {
+	var edge = properties.edges[0];
+	var node = properties.nodes[0];
+	
+	if ( shifted ) {
+		if ( properties.edges != '' && properties.nodes == '') {
+			editEdge( edge );
+		} else if (properties.nodes != ''){
+			$('#dialog').dialog('open');
+			
+			document.getElementById('dialog-title').innerHTML = 'Edit Node';
+			document.getElementById('dialog-id-label').innerHTML = 'ID: ';
+			document.getElementById('dialog-id').innerHTML = nodes.get(node).id;
+			document.getElementById('dialog-name').value = nodes.get(node).label;
+			if ( nodes.get(node).group == 'Collaboration' ) {
+				document.getElementById('dialog-type-collaboration').checked = true;
+			} else if ( nodes.get(node).group == 'Controller' ) {
+				document.getElementById('dialog-type-controller').checked = true;
+			} else if ( nodes.get(node).group == 'Database' ) {
+				document.getElementById('dialog-type-database').checked = true;
+			}
+			document.getElementById('dialog-uri').value = arbNodeData.get(node).uri;
+			document.getElementById('dialog-description').value = arbNodeData.get(node).description;
+			
+			isNodeAdd = false;
+			isNodeEdit = true;
+		}
+	} 
+	shifted = false;
 });
 
 
